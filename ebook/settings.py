07f9ea4acb5ef14ff46eb680f1d9203ef2e08cca
David1099 <davidmorales639@yahoo.com>
2021-03-28 16:29:18 -0400

    password change

--- ebook/settings.py
+++ ebook/settings.py
@@ -127,7 +127,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'infoebooksconnection@gmail.com'
-EMAIL_HOST_PASSWORD = 'Justchillin223#$'
+EMAIL_HOST_PASSWORD = 'Justchillin223!'
 EMAIL_USE_TLS = True 
 
 
